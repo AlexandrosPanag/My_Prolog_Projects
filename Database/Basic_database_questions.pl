21d790df774032248593092faeca5dec842e6aa0
Αλέξανδρος Παναγιωτακόπουλος <69578661+AlexandrosPanag@users.noreply.github.com>
2021-06-21 15:24:57 +0300

    Add files via upload

---
+++ Database/Basic_database_questions.pl
@@ -0,0 +1,7 @@
+?-born(maria,X). %asking the date that maria was born
+
+?-born(nicole,date(X,Y,Z)). %asking the date,the month and the year that nicole was born
+
+?-born(helen,date(_,_,Z)). %asking the year that helen was born in
+
+
