d6d675b89dd5e8e86464a514e1fdb7b947793be0
Αλέξανδρος Παναγιωτακόπουλος <69578661+AlexandrosPanag@users.noreply.github.com>
2021-06-21 14:36:53 +0300

    Add files via upload

---
+++ Family_Tree/Family_Tree.pl
@@ -0,0 +1,14 @@
+parent(tim,ross).
+parent(tim,james).
+parent(ross,alan).
+parent(ross,kanye).
+parent(james,chris).
+parent(alan,stuart).
+
+male(tim).
+male(james).
+male(alan).
+male(ross).
+male(chris).
+male(stuart).
+male(kanye).
