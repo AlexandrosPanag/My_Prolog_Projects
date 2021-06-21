5a31de5b22eca05bea1d0610973bd9fa2fbc7773
Αλέξανδρος Παναγιωτακόπουλος <69578661+AlexandrosPanag@users.noreply.github.com>
2021-06-21 15:08:18 +0300

    Add files via upload

---
+++ Database_example_with_date.pl
@@ -0,0 +1,14 @@
+parent(tim,ross).
+parent(tim,james).
+parent(ross,alan).
+parent(ross,kanye).
+parent(james,chris).
+parent(alan,stuart).
+
+born(tim,data(12,11,1923)).
+born(james,data(12,9,1953)).
+born(ross,data(12,10,1947)). 
+
+%etc..etc
+
+
