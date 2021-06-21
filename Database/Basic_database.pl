a645d555b068e61be966917d330660ba5155cd22
Αλέξανδρος Παναγιωτακόπουλος <69578661+AlexandrosPanag@users.noreply.github.com>
2021-06-21 15:20:32 +0300

    Add files via upload

---
+++ Database/Basic_database.pl
@@ -0,0 +1,16 @@
+male(john).
+male(chris).
+male(tim).
+female(maria).
+female(nicole).
+female(helen).
+
+born(john,date(24,11,1923)).
+born(chris,date(16,9,1999)).
+born(tim,date(10,10,1987)). 
+
+born(maria,date(13,10,1995)).
+born(nicole,date(15,8,2000)).
+born(helen,date(17,7,1990)).
+
+
