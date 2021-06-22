1e3758d3f0b09aa5d56d5fd2004827253d875904
Αλέξανδρος Παναγιωτακόπουλος <69578661+AlexandrosPanag@users.noreply.github.com>
2021-06-22 18:03:00 +0300

    Add files via upload

---
+++ Arithmetic_Operations_In_Prolog.pl
@@ -0,0 +1,13 @@
+?- X is 2*3. %the result will be multiplied
+
+
+
+?- X is 2+3.  %the result will be added
+
+
+
+
+?- X is 6/3.  %the result will be added
+
+
+?- X is 6-3. %the result will be subtracted
